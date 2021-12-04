5f1b686da4cdd7f31b4bf5a8b3dd9860c11e20a1
Brittany Moliver <britt.moliver@gmail.com>
2021-12-04 00:12:12 -0500

    made login and sign up Modals

---
+++ frontend/src/components/Navigation/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import ProfileButton from './ProfileButton';
+import LoginFormModal from '../LoginFormModal';
+import SignupFormModal from '../SignupFormModal';
+import './Navigation.css';
+
+function Navigation({ isLoaded }){
+  const sessionUser = useSelector(state => state.session.user);
+
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = (
+      <ProfileButton user={sessionUser} />
+    );
+  } else {
+    sessionLinks = (
+      <>
+        {/* <NavLink to="/login">Log In</NavLink> */}
+        <LoginFormModal />
+        {/* <NavLink to="/signup">Sign Up</NavLink> */}
+        <SignupFormModal />
+      </>
+    );
+  }
+
+  return (
+    <ul>
+      <li>
+        <NavLink exact to="/">Home</NavLink>
+        {isLoaded && sessionLinks}
+      </li>
+    </ul>
+  );
+}
+
+export default Navigation;
