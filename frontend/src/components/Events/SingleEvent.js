817aade554314fe7354cb93be9593286ec24d458
Brittany Moliver <britt.moliver@gmail.com>
2021-12-07 19:23:17 -0500

    added edit event button only to hosts

--- frontend/src/components/Events/SingleEvent.js
+++ frontend/src/components/Events/SingleEvent.js
@@ -2,6 +2,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getSingleEvent } from '../../store/event'
 import { useEffect } from 'react';
+// import EditEventForm from './EditEventForm';
 
 
 export default function SingleEvent() {
@@ -12,15 +13,27 @@ export default function SingleEvent() {
         dispatch(getSingleEvent(eventId))
     }, [dispatch, eventId])
 
-
-    const event = useSelector(store => store.eventReducer);
+    const event = useSelector(store => store.eventReducer[eventId]);
     // console.log('event =', event)
-    // console.log('event?[eventId] =', event[eventId])
+
+    const sessionUser = useSelector(state => state.session.user);
+    // console.log('sessionUser', sessionUser?.id)
+    // console.log('event.hostId', event?.hostId)
+    // console.log('do they equal?', sessionUser?.id === event?.hostId)
+
+    let sessionLinks;
+    if (sessionUser && sessionUser?.id === event?.hostId) {
+      sessionLinks = (
+        <>
+            <button>Edit Event</button>
+        </>
+      );
+    }
 
     return (
         <>
-            {/* <h2>{event.title}</h2> */}
-            <h2>Event: {eventId}</h2>
+            <h2>{event?.title}</h2>
+            {sessionLinks}
         </>
     )
 }
