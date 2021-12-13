a3c5cd08f1e4d4bfaaf4709b680c95fc4242b248
Brittany Moliver <britt.moliver@gmail.com>
2021-12-13 10:51:14 -0500

    small edits nav bar and splash page

--- frontend/src/components/SplashPage/SplashPage.js
+++ frontend/src/components/SplashPage/SplashPage.js
@@ -1,12 +1,30 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import DemoUser from '../DemoUser/DemoUser';
+import SignupFormModal from '../SignupFormModal';
+import LoginFormModal from '../LoginFormModal';
+
+import '../Navigation/Navigation.css';
 import './splashpage.css'
 
 export default function SplashPage() {
     return (
-        <div className='splashPage'>
-            <Link className='splashPageLink' to='/events' style={{ textDecoration: 'none' }}>
-                <h1 className='splashPageHeader'>Ficticious Festivities!!!</h1>
-            </Link>
-        </div>
+        <>
+            <nav style={{justifyContent: 'space-evenly'}}>
+                <SignupFormModal />
+                <LoginFormModal />
+                <DemoUser />
+                <NavLink exact to="/events">
+                    <button className="tickets-button">
+                        <i className="fas fa-user-circle" />
+                        Enter as Guest
+                    </button>
+                </NavLink>
+            </nav>
+            <div className='splashPage'>
+                <Link className='splashPageLink' to='/events' style={{ textDecoration: 'none' }}>
+                    <h1 className='splashPageHeader'>Ficticious Festivities!!!</h1>
+                </Link>
+            </div>
+        </>
     )
 }
